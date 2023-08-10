a9cdd6951e2dc5766a42c02ead8d639eaadfe3ef
takeaname1o1 <jitenkr129152@gmail.com>
2023-08-10 22:46:24 +0530

    comments

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # invert pdf color without quality loss
+
 usage  - >> 
 path/to/invert.sh  path/to/pdf  
+
+dependency -> pdftocairo ,  imagemagick , img2pdf , evince (not mandetory)
+
+compatible OS -> LINUX all distro  
--- invert.sh
+++ invert.sh
@@ -1,15 +1,27 @@
-echo started 
+echo started  
+#print started
 pdftocairo -png -r 300 "$1" img
+#converts pdf to img (png) using pdftocairo
 echo image extraction done
+#print extraction done
 convert img*.png -channel RGB -negate invert.png
+#invert image using  imagemagick
 echo image inverted
+#print image inverted
 rm img-*.png
+#delete extracted image
 echo png deleted
-img2pdf invert-*.png  -o output.pdf
+#print deleted png
+img2pdf invert-*.png  -o inverted.pdf
+#convert image to pdf using img2pdf
 echo pdf created 
+#print pdf created
 rm invert-*.png
+#delete inverted png
 echo all png deleted check output
- evince output.pdf
+#print png deleted
+evince inverted.pdf
+#lunch inverted using evince 
 
 
 
